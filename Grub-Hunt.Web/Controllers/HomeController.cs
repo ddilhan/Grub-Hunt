9b6a5c279f96d5a82310bfb9a535c5308c187a2e
Darshana Dilhan <47178813+ddilhan@users.noreply.github.com>
2023-12-28 15:39:52 +0530

    web project initial commit

---
+++ Grub-Hunt.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Grub_Hunt.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Grub_Hunt.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Grub-Hunt.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Grub_Hunt.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
