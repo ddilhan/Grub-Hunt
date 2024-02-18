4213583ad771891adde2767874b1f2492fc46c1a
Darshana Dilhan <47178813+ddilhan@users.noreply.github.com>
2024-02-18 21:38:58 +0530

    Create StaticDetails.cs

---
+++ Grub-Hunt.Web/Utility/StaticDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Grub_Hunt.Web.Utility
+{
+    public static class StaticDetails
+    {
+        public static string? AuthAPIBaseUrl;
+        public const string? RoleAdmin = "ADMIN";
+        public const string? RoleCustomer = "CUSTOMER";
+        public const string? TokenCookie = "AccessToken";
+    }
+}
