250828c3a8959d0337388a45796f335ddc9f431a
Darshana Dilhan <47178813+ddilhan@users.noreply.github.com>
2024-05-19 18:45:56 +0530

    add product API

--- Grub-Hunt.ProductAPI/Program.cs
+++ Grub-Hunt.ProductAPI/Program.cs
@@ -1,10 +1,11 @@
-using AutoMapper;
 using Grub_Hunt.ProductAPI.Data;
-using Grub_Hunt.ProductAPI.DTOs;
 using Grub_Hunt.ProductAPI.Implementations;
 using Grub_Hunt.ProductAPI.Interfaces;
-using Grub_Hunt.ProductAPI.Models;
+using Grub_Hunt.ProductAPI.Utility;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,12 +18,37 @@ builder.Services.AddDbContext<AppDBContext>(options =>
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddSingleton<ConvertObjects>();
 builder.Services.AddTransient<IProductService, ProductService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var secret = builder.Configuration.GetValue<string>("APISettings:JWTOptions:Secret");
+var issuer = builder.Configuration.GetValue<string>("APISettings:JWTOptions:Issuer");
+var audience = builder.Configuration.GetValue<string>("APISettings:JWTOptions:Audience");
+
+var key = Encoding.ASCII.GetBytes(secret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => 
+{
+    x.TokenValidationParameters = new()
+    {
+        ValidateIssuerSigningKey = false,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidIssuer = issuer,
+        ValidateAudience = false,
+        ValidAudience = audience,
+    };
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -35,8 +61,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
